ca15e5f83c33e332be76661be33a5578ac3dc5a9
Albert Salimov <salimovalbert99@yandex.ru>
2026-06-17 02:13:40 +0500

    preparing to deploy

--- CyberFraudQuiz/CyberFraudQuiz/settings.py
+++ CyberFraudQuiz/CyberFraudQuiz/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^r4jd1l#fk%v)y0g)@nqjo7#8!t7+mpwf4qv588-h%cp7g-yx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.10.101']
 
 # Application definition
 
@@ -119,3 +119,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
